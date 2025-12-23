df72beaca17ef4463dd1935ce9f880165f2a0469
AbhijitGaikwad <abhijit.gaikwad@neutrinotechlabs.com>
2025-12-24 00:04:33 +0530

    adding crud app 4

--- services/user_service.py
+++ services/user_service.py
@@ -22,12 +22,12 @@ def get_all_users_service():
     return users, 200
 
 def update_user_service(user_id, data):
-    # if user_id not in users:
-    #     return {"message": "User not found"}, 404
+    if user_id not in users:
+        return {"message": "User not found"}, 404
 
-    # if "name" in data:
-    #     users[user_id]["name"] = data["name"]
-    # if "email" in data:
-    #     users[user_id]["email"] = data["email"]
+    if "name" in data:
+        users[user_id]["name"] = data["name"]
+    if "email" in data:
+        users[user_id]["email"] = data["email"]
 
     return {"message": "User updated", "user": users[user_id]}, 200
