d2d88b5c4d92c0274c4d9452156f881078cdb479
AbhijitGaikwad <abhijit.gaikwad@neutrinotechlabs.com>
2025-12-23 12:25:21 +0530

    adding crud app

---
+++ routes/user_routes.py
@@ -0,0 +1,26 @@
+
+from flask import Blueprint, request, jsonify
+from services.user_service import (
+    create_user_service,
+    get_user_service,
+    get_all_users_service,
+    update_user_service
+)
+
+user_bp = Blueprint("users", __name__)
+
+@user_bp.route("/users", methods=["POST"])
+def create_user():
+    return create_user_service(request.get_json())
+
+@user_bp.route("/users/<int:user_id>", methods=["GET"])
+def get_user(user_id):
+    return get_user_service(user_id)
+
+@user_bp.route("/users", methods=["GET"])
+def get_all_users():
+    return get_all_users_service()
+
+@user_bp.route("/users/<int:user_id>", methods=["PUT"])
+def update_user(user_id):
+    return update_user_service(user_id, request.get_json())
