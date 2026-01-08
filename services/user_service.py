365da0ee45f47436199da1af66b3d04351eabc8b
AbhijitGaikwad <abhijit.gaikwad@neutrinotechlabs.com>
2026-01-08 17:35:26 +0530

    demo commit 1

--- services/user_service.py
+++ services/user_service.py
@@ -17,8 +17,7 @@ def get_user_service(user_id):
         return {"message": "User not found"}, 404
     return user, 200
 
-
-
+# demo commit 1
 
 def get_all_users_service():
     return users, 200
