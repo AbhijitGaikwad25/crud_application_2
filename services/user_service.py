068e578b4d059b378682c0e50f29c48f9bf87842
Abhijit Gaikwad <abhijit.gaikwad@neutrinotechlabs.com>
2026-03-24 11:53:37 +0000

    demo check 138

--- services/user_service.py
+++ services/user_service.py
@@ -46,3 +46,6 @@ def update_user_service(user_id, data):
         return {"message": "User updated successfully", "user": users[user_id]}, 200
     except Exception as e:
         return {"message ": "An error occurred: " + str(e)}, 500
+
+def demo_service():
+    return {"message": "This is a demo service response."}, 200
